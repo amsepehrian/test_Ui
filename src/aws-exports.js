e480715d45fa04e229a4e66cd2331f4d6391c5d2
amsepehrian <117947230+amsepehrian@users.noreply.github.com>
2023-07-14 15:23:06 +0330

    Update aws-exports.js
    change on configs

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,16 +2,16 @@
 const awsmobile = {
 	Auth: {
 		region: 'eu-north-1',
-		userPoolId: 'eu-north-1_zNLiOUNKU',
-		userPoolWebClientId: '7e3tn6hnusu6f5b5tsu1f53ov',
+		userPoolId: 'eu-north-1_wPxaUpdnH',
+		userPoolWebClientId: '59vm5n9i86m8tn4kvsca9eenpc',
 		//userPoolWebClientId: '6vstsucid387qyiry59360',
         //userPoolWebClientId: '6vstsuciYOURCLIENTID',
-		identityPoolId: 'eu-north-1:2cce83c7-ccd0-48db-aaec-ab070c27d906',
+		identityPoolId: 'eu-north-1:cef88f62-52ba-4c88-990d-f79607afce40',
 		
 	},
 	Storage: {
 		AWSS3: {
-			bucket: 'amplify-front-dev-123621-deployment',
+			bucket: 'filestoragestack-s3bucket64cb25af-y5q4mf4snro5',
 			region: 'eu-north-1',
 		},
 	},
